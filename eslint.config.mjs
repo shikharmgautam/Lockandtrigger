c9aa3e9a20f61097a26a82d41c13b235efed9420
Shikhar Mohan Gautam <64278897+shikharmgautam@users.noreply.github.com>
2026-02-13 15:04:16 +0530

    bad format

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
-function test( ){console.log("hello")}
+function 
+  test( ){console.log("hello")}
 
 const eslintConfig = defineConfig([
   ...nextVitals,
